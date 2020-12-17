2bdaf945e50cf5a7f6163b2bf9c40bf83ed80705
krae <kraewind@gmail.com>
2020-12-17 14:27:58 -0500

    commit tree ed91d1bbedab872068b7c123f1650e9c48faebd6

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ('The King Killer Chronicles', 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ('The King Killer Chronicles', 1, 1);
+
+INSERT INTO books (title, year, series_id) VALUES ('The Name Of The Wind', 2015, 1);
+INSERT INTO books (title, year, series_id) VALUES ('The Name Of The Wind', 2015, 1);
+INSERT INTO books (title, year, series_id) VALUES ('The Name Of The Wind', 2015, 1);
+INSERT INTO books (title, year, series_id) VALUES ('The Name Of The Wind', 2015, 1);
+INSERT INTO books (title, year, series_id) VALUES ('The Name Of The Wind', 2015, 1);
+INSERT INTO books (title, year, series_id) VALUES ('The Name Of The Wind', 2015, 1);
+
+INSERT INTO characters (name, motto, species, author_id) VALUES ('Kvothe', 'I will destroy the Chandrian', 'Human', 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ('Kvothe', 'I will destroy the Chandrian', 'Human', 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ('Kvothe', 'I will destroy the Chandrian', 'Human', 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ('Kvothe', 'I will destroy the Chandrian', 'Human', 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ('Kvothe', 'I will destroy the Chandrian', 'Human', 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ('Kvothe', 'I will destroy the Chandrian', 'Human', 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ('Kvothe', 'I will destroy the Chandrian', 'Human', 1);
+INSERT INTO characters (name, motto, species, author_id) VALUES ('Kvothe', 'I will destroy the Chandrian', 'Human', 1);
+
+INSERT INTO subgenres (name) VALUES ('Shonen');
+INSERT INTO subgenres (name) VALUES ('Shonen');
+
+INSERT INTO authors (name) VALUES ('Patrick Rothfuss');
+INSERT INTO authors (name) VALUES ('Patrick Rothfuss');
+
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
